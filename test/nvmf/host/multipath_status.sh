99cfab48f6d5d950861e7703a4ce1859cf3ab9de
Jim Harris <jiharris@nvidia.com>
2026-02-26 04:06:28 -0700

    test/nvmf: cache io_paths RPC result in multipath_status
    check_status calls port_status 6 times, each spawning a
    separate rpc.py process to query the same bdev_nvme_get_io_paths
    data. Cache the JSON result in check_status and run the jq
    checks against the cached output, reducing 6 rpc.py spawns
    per check_status invocation to 1.
    
    Change-Id: I4307c7caaf4d3bb0a2117da9ba961afdc650ae8e
    Signed-off-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27926
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>

--- test/nvmf/host/multipath_status.sh
+++ test/nvmf/host/multipath_status.sh
@@ -58,17 +58,16 @@ function set_ANA_state() {
 	$rpc_py nvmf_subsystem_listener_set_ana_state $NQN -t $TEST_TRANSPORT -a $NVMF_FIRST_TARGET_IP -s $NVMF_SECOND_PORT -n $2
 }
 
-function port_status() {
-	[[ $($rpc_py -s $bdevperf_rpc_sock bdev_nvme_get_io_paths | jq -r ".poll_groups[].io_paths[] | select (.transport.trsvcid==\"$1\").$2") == "$3" ]]
-}
-
 function check_status() {
-	port_status $NVMF_PORT current $1
-	port_status $NVMF_SECOND_PORT current $2
-	port_status $NVMF_PORT connected $3
-	port_status $NVMF_SECOND_PORT connected $4
-	port_status $NVMF_PORT accessible $5
-	port_status $NVMF_SECOND_PORT accessible $6
+	local io_paths
+	io_paths=$($rpc_py -s $bdevperf_rpc_sock bdev_nvme_get_io_paths)
+
+	[[ $(jq -r ".poll_groups[].io_paths[] | select(.transport.trsvcid==\"$NVMF_PORT\").current" <<< "$io_paths") == "$1" ]]
+	[[ $(jq -r ".poll_groups[].io_paths[] | select(.transport.trsvcid==\"$NVMF_SECOND_PORT\").current" <<< "$io_paths") == "$2" ]]
+	[[ $(jq -r ".poll_groups[].io_paths[] | select(.transport.trsvcid==\"$NVMF_PORT\").connected" <<< "$io_paths") == "$3" ]]
+	[[ $(jq -r ".poll_groups[].io_paths[] | select(.transport.trsvcid==\"$NVMF_SECOND_PORT\").connected" <<< "$io_paths") == "$4" ]]
+	[[ $(jq -r ".poll_groups[].io_paths[] | select(.transport.trsvcid==\"$NVMF_PORT\").accessible" <<< "$io_paths") == "$5" ]]
+	[[ $(jq -r ".poll_groups[].io_paths[] | select(.transport.trsvcid==\"$NVMF_SECOND_PORT\").accessible" <<< "$io_paths") == "$6" ]]
 }
 
 "$rootdir/examples/bdev/bdevperf/bdevperf.py" -t 120 -s $bdevperf_rpc_sock perform_tests &
